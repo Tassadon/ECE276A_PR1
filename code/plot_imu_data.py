0ada63d7278933ea87bc37945db0d281b09a6978
Tassadon <bananaman2two@gmail.com>
2024-02-05 14:37:14 -0800

    A

--- code/plot_imu_data.py
+++ code/plot_imu_data.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import autograd.numpy as np
 import transforms3d
 
-def plot(dataset="1",path="../.."):
+def plot(dataset="1",path="../numpy files"):
     quarternions_T = np.load(path + "/data_set_" + dataset + "_raw_quarternions.npy")
     optimized_qts = np.load(path + "/data_set_" + dataset + "_optimized_quarternions.npy")
-    rots = np.load("../numpy files/data_set_" + dataset + "_vicon_data.npy")
+    rots = np.load(path + "/data_set_" + dataset + "_vicon_data.npy")
 
     euler_angles = list(map(lambda quart: transforms3d.euler.quat2euler(quart),quarternions_T))
     roll = list(map(lambda angles: angles[0],euler_angles))
@@ -18,13 +18,13 @@ def plot(dataset="1",path="../.."):
     opt_pitch = list(map(lambda angles: angles[1],opt_euler_angles))
     opt_yaw = list(map(lambda angles: angles[2],opt_euler_angles))
 
-    fig, ax = plt.subplots(3,1,figsize=(10,10))
+    fig, ax = plt.subplots(3,1,figsize=(6,6))
     ax[0].plot(roll)
     ax[0].set_title("roll in radians")
     ax[0].plot(opt_roll)
     ax[0].plot(rots[:,0])
     ax[1].plot(pitch)
-    ax[1].set_title("yaw in radians")
+    ax[1].set_title("yaw in radians",x=.2,y=0)
     ax[1].plot(opt_pitch)
     ax[1].plot(rots[:,1])
     ax[2].plot(yaw)
@@ -35,3 +35,7 @@ def plot(dataset="1",path="../.."):
     ax[1].legend(['raw data','optimized data','VICON data'],loc='lower right')
     ax[2].legend(['raw data','optimized data','VICON data'],loc='lower right')
     fig.suptitle('Yaw Pitch and Roll calculations from dataset: ' + dataset)
+    plt.show()
+
+if __name__ == "__main__":
+    plot(dataset="2")
