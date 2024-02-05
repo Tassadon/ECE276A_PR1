17723012216fea9d94807d2fb9cdd7c0d67455f2
Tassadon <bananaman2two@gmail.com>
2024-02-04 22:23:17 -0800

    asdasd

--- code/optimization.py
+++ code/optimization.py
@@ -4,44 +4,49 @@ import quarternions
 
 def myCostFunc(quarts,Acc,tau,omega):
 
-    a = []
+    
     #print(quarts.shape[0])
+    a = []
     for quart in quarts:
         a.append(quarternions.get_a(quart))
     a = jnp.array(a)
     #print(a[:,1:].shape)
     #print(Acc.shape)
+    
     c2 = .5*jnp.sum(jnp.square(jnp.linalg.norm(Acc - a[:,1:],)))
 
     c1 = []
 
     for i in range(1,quarts.shape[0]):
         funko = quarternions.q_multiply(quarternions.inverse(quarts[i]),quarternions.next_quarternion(quarts[i-1],tau[i-1],omega[i-1]))
-        fun = 2*jnp.log(funko)
+        fun = 2*quarternions.log(funko+10**-6)
         c1.append(jnp.square(jnp.linalg.norm(fun)))
     c1 = jnp.sum(jnp.array(c1))
     cost = c1 + c2
 
     return cost
 
-def update_qts(quarts, gradient,alpha=.1):
+def update_qts(quarts, gradient,alpha=.01):
     quarts = quarts + alpha*gradient
     return quarts
 
-def optimize(quarts, Acc,tau,omega,alpha=.1):
+def optimize(quarts, Acc,tau,omega,alpha=.01):
 
-    gradFunc = jax.jacfwd(myCostFunc)
+    gradFunc = jax.jacrev(myCostFunc)
     
     for i in range(10):
+        print(i)
         quarts = quarts/jnp.array([jnp.linalg.norm(quarts,axis=1)]).T
-        gradient = gradFunc(quarts,Acc,tau,omega)
-        quarts = update_qts(quarts, gradient,alpha=alpha)
-        
-        print("Loss at iteration %d: %.2f" % (i+1, myCostFunc(quarts,Acc,tau,omega)))
+        quarts = quarts - alpha*gradFunc(quarts,Acc,tau,omega)
+        print(quarts)
+        print(myCostFunc(quarts,Acc,tau,omega))
+        #print("Loss at iteration %d: %.2f" % (i+1, myCostFunc(dingo,Acc,tau,omega)))
+
+    quarts = quarts/jnp.array([jnp.linalg.norm(quarts,axis=1)]).T
 
     return quarts
 
-
+    
 
 if __name__ == "__main__":
     joe = jnp.array([[1,2,3,4],[1,5,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]],dtype=jnp.float32)
@@ -50,11 +55,11 @@ if __name__ == "__main__":
     acc = jnp.array([[1,1,1],[1,1,1],[1,1,1],[1,1,1],[1,1,1]],dtype=jnp.float32)
     #print(joe/jnp.array([jnp.linalg.norm(joe,axis=1)]).T)
     #print(myCostFunc(joe,acc,tau,omega))
-    subu = jax.jacfwd(myCostFunc,argnums=0)
-    print(joe.shape)
-    print(tau.shape)
-    print(omega.shape)
-    print(acc.shape)
+    subu = jax.jacrev(myCostFunc,argnums=0)
+    #print(joe.shape)
+    #print(tau.shape)
+    #print(omega.shape)
+    #print(acc.shape)
     #print(subu(joe,acc,tau,omega))
-
-    optimize(joe,acc,tau,omega)
+    #print(myCostFunc(joe,acc,tau,omega))
+    print(optimize(joe,acc,tau,omega))
--- code/quarternions.py
+++ code/quarternions.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 
+
 def predict_next_quarternion(tau,omega):
     assert omega.shape[0] == 3
     q0 = jnp.array([1,0,0,0],dtype=jnp.float32)
@@ -17,6 +18,7 @@ def predict_next_quarternion(tau,omega):
 
     return qt
 
+
 def next_quarternion(q,tau,omega):
     tau_t = tau/2
     omega_t = omega*tau_t
@@ -24,8 +26,10 @@ def next_quarternion(q,tau,omega):
     qProduct = q_multiply(q,fun)
     return qProduct
 
+
 def q_norm(q):
     return jnp.sqrt(q[0]**2 + jnp.dot(q,q))
+
 
 def q_multiply(q1,q2):
     qs = q1[0]*q2[0] - jnp.dot(q1[1:],q2[1:])
@@ -33,29 +37,38 @@ def q_multiply(q1,q2):
     qv = q1[0]*q2[1:] + q2[0]*q1[1:] + the_cross
     return jnp.array([qs, qv[0], qv[1],qv[2]],dtype=jnp.float32)
 
+
 def exponential(q):
     scalar = jnp.exp(q[0])
     qs = jnp.cos(q_norm(q[1:]))
     qv = q[1:]*jnp.sin(q_norm(q[1:]))/q_norm(q[1:])
     return jnp.array([qs,qv[0],qv[1],qv[2]])*scalar
 
+
 def conjugate(q):
     return jnp.array([q[0],-q[1],-q[2],-q[3]])
 
+
 def inverse(q):
     return conjugate(q)/(jnp.square(q_norm(q)))
+
 
 def get_a(q):
 
     return q_multiply(q_multiply(inverse(q),jnp.array([0,0,0,-9.81])),q)
 
+def log(q):
+    qv = q[1:]*jnp.arccos(q[0]/jnp.linalg.norm(q))/jnp.linalg.norm(q)
+    DOOBO = jnp.array([jnp.log(q_norm(q)),qv[0],qv[1],qv[2]])
+    return DOOBO
+
 if __name__ == "__main__":
     tau = jnp.array([[1],[1],[1],[1]])
     omega = jnp.array([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
-    q = jnp.array([1,0,1,0])
+    q = jnp.array([1,0,0,0])
     #print(omega.T.shape)
     G = predict_next_quarternion(tau,omega.T)
     Z = jnp.array(G)
     #print(Z.shape)
-    print(get_a(q))
-    
+    #print(get_a(q))
+    print(":LSKJFG:LKSDJG:LKJ AHHHHH:", log(q))
